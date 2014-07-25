3603be3d647e53fd325cf2500520a75db2ced64d
Christopher Woolum <Christopher.Woolum@gmail.com>
2014-07-25 15:43:52 -0700

    Add method for retrieval of trackers and unit test

--- EasyPost/Tracker.cs
+++ EasyPost/Tracker.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EasyPost {
-    public class Tracker : IResource {
-        public string id { get; set; }
+namespace EasyPost
+{
+    public class Tracker : IResource
+    {
+        private static Client client = new Client();
+
         public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
-        public List<TrackingDetail> tracking_details { get; set; }
-        public string tracking_code { get; set; }
-        public string status { get; set; }
-        public string shipment_id { get; set; }
+
+        public string id { get; set; }
+
         public string mode { get; set; }
+
+        public string shipment_id { get; set; }
+
+        public string status { get; set; }
+
+        public string tracking_code { get; set; }
+
+        public List<TrackingDetail> tracking_details { get; set; }
+
+        public DateTime updated_at { get; set; }
+
+        public static Tracker Retrieve(string carrier, string trackingCode)
+        {
+            Request request = new Request("trackers", RestSharp.Method.POST);
+            var requestParams = new Dictionary<string, object>();
+           
+            requestParams.Add("tracking_code", trackingCode);
+            requestParams.Add("carrier", carrier);
+            request.addBody(requestParams, "tracker");
+
+            return client.Execute<Tracker>(request);
+        }
     }
 }
