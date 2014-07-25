e3d7e73473314aaf9c33cf0a2803c875ac09f550
Christopher Woolum <Christopher.Woolum@gmail.com>
2014-07-25 16:28:53 -0700

    cut down newlines and make the dictionary initializer one line.

--- EasyPost/Tracker.cs
+++ EasyPost/Tracker.cs
@@ -8,29 +8,21 @@ namespace EasyPost
         private static Client client = new Client();
 
         public DateTime created_at { get; set; }
-
         public string id { get; set; }
-
         public string mode { get; set; }
-
         public string shipment_id { get; set; }
-
         public string status { get; set; }
-
         public string tracking_code { get; set; }
-
         public List<TrackingDetail> tracking_details { get; set; }
-
         public DateTime updated_at { get; set; }
 
         public static Tracker Retrieve(string carrier, string trackingCode)
         {
             Request request = new Request("trackers", RestSharp.Method.POST);
-            var requestParams = new Dictionary<string, object>();
-           
-            requestParams.Add("tracking_code", trackingCode);
-            requestParams.Add("carrier", carrier);
-            request.addBody(requestParams, "tracker");
+            Dictionary<string, object> parameters = new Dictionary<string, object>() { 
+                { "tracking_code", trackingCode }, { "carrier", carrier } };
+
+            request.addBody(parameters, "tracker");
 
             return client.Execute<Tracker>(request);
         }
