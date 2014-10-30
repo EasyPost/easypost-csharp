d69558fcbb2261bd74b0d6bd71bd07b87f4701fd
jmalatia <jmalatia@users.noreply.github.com>
2014-10-30 18:39:34 -0500

    Add missing properties to CustomsInfo.cs
    Added properties:
    - contents_type
    - restriction_comments

--- EasyPost/CustomsInfo.cs
+++ EasyPost/CustomsInfo.cs
@@ -13,12 +13,14 @@ namespace EasyPost {
         public string id { get; set; }
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
+        public string contents_type { get; set; }
         public string contents_explanation { get; set; }
         public string customs_certify { get; set; }
         public string customs_signer { get; set; }
         public string eel_pfc { get; set; }
         public string non_delivery_option { get; set; }
         public string restriction_type { get; set; }
+        public string restriction_comments { get; set; }
         public List<CustomsItem> customs_items { get; set; }
         public string mode { get; set; }
 
