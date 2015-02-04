a155552e9772d6e283b6c4f9e92e42ce87446bdd
Andrew Tribone <tribone@easypost.com>
2015-02-03 16:48:02 -0800

    Update OrderTest

--- EasyPostTest/OrderTest.cs
+++ EasyPostTest/OrderTest.cs
@@ -16,12 +16,22 @@ namespace EasyPostTest {
             Client.apiKey = "cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi";
 
             toAddress = new Dictionary<string, object>() {
-                {"company", "Simpler Postage Inc"}, {"street1", "164 Townsend Street"}, {"street2", "Unit 1"},
-                {"city", "San Francisco"}, {"state", "CA"}, {"country", "US"}, {"zip", "94107"},
+                {"company", "Simpler Postage Inc"},
+                {"street1", "164 Townsend Street"},
+                {"street2", "Unit 1"},
+                {"city", "San Francisco"},
+                {"state", "CA"},
+                {"country", "US"},
+                {"zip", "94107"},
             };
             fromAddress = new Dictionary<string, object>() {
-                {"name", "Andrew Tribone"}, {"street1", "480 Fell St"}, {"street2", "#3"},
-                {"city", "San Francisco"}, {"state", "CA"}, {"country", "US"}, {"zip", "94102"}
+                {"name", "Andrew Tribone"},
+                {"street1", "480 Fell St"},
+                {"street2", "#3"},
+                {"city", "San Francisco"},
+                {"state", "CA"},
+                {"country", "US"},
+                {"zip", "94102"}
             };
             shipments = new List<Dictionary<string, object>>() {
                 new Dictionary<string, object>() {    
@@ -31,26 +41,21 @@ namespace EasyPostTest {
                     {"parcel", new Dictionary<string, object>() {{"length", 9}, {"width", 5}, {"height", 4}, {"weight", 18}}}
                 }
             };
+
             parameters = new Dictionary<string, object>() {
-                {"to_address", toAddress}, {"from_address", fromAddress}, {"reference", "ShipmentRef"}, {"shipments", shipments}
+                {"to_address", toAddress},
+                {"from_address", fromAddress},
+                {"reference", "OrderRef"},
+                {"shipments", shipments}
             };
         }
-
-        //private Order createOrderResource() {
-        //    Address to = Address.Create(toAddress);
-        //    Address from = Address.Create(fromAddress);
-        //    List<Container> containers = new List<Container>() {};
-        //    List<Item> items = new List<Item>() { };
-
-        //    return new Order() {to_address = to, from_address = from, containers = containers, items = items};
-        //}
 
         [TestMethod]
         public void TestCreateAndRetrieveOrder() {
             Order order = Order.Create(parameters);
 
             Assert.IsNotNull(order.id);
-            Assert.AreEqual(order.reference, "ShipmentRef");
+            Assert.AreEqual(order.reference, "OrderRef");
 
             Order retrieved = Order.Retrieve(order.id);
             Assert.AreEqual(order.id, retrieved.id);
@@ -63,13 +68,20 @@ namespace EasyPostTest {
             order.Create();
         }
 
-        // Can implement when we have items and containers working properly
-        //[TestMethod]
-        //public void TestCreateOrderWithPreCreatedAttributes() {
-        //    Order order = createOrderResource();
-        //    order.Create();
-        //    Assert.IsNotNull(order.id);
-        //}
+        [TestMethod]
+        public void TestCreateFromInstance() {
+            Order order = new Order() {
+                to_address = Address.Create(toAddress),
+                from_address = Address.Create(fromAddress),
+                reference = "OrderRef",
+                shipments = shipments.Select(shipment => Shipment.Create(shipment)).ToList()
+            };
+
+            order.Create();
+
+            Assert.IsNotNull(order.id);
+            Assert.AreEqual(order.reference, "OrderRef");
+        }
 
         [TestMethod]
         public void TestBuyOrder() {
