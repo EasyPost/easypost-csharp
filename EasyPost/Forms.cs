d36f650c52d2531fe59be9ee77205a96c92774f9
DarkChowder <DarkChowder@hotmail.com>
2015-03-27 14:11:30 -0700

    Added forms to shipments
    I have created a pull request based on the changed by tig4647. It adds
    forms to the shipment class, and correctly adds forms to the project
    files.
    This closes #22
    This pull request implements pull request numbers #23, and #24

---
+++ EasyPost/Forms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EasyPost
+{
+    public class Forms : IResource
+    {
+        public string id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }      
+        public string form_url { get; set; }
+        public string form_type { get; set; }
+        public string mode { get; set; }
+    }
+}
