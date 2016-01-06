d871859c434a9a7aa5d54274440cf9d7b574fc18
kcamp <campkd@gmail.com>
2016-01-06 10:35:05 -0500

    Extend the Client object to accept ClientConfiguration options for apiKey assigment
    This provides the ability to pass the API key and URL on a per-client basis,
    making the library more flexible in a multi-account environment.
    
    Additionally, refactored the base url value to an internally visible constant;
    this provides consistent assignment of the value in the Client + ClientConfiguration classes
    while maintaining backward compatibility.

--- EasyPostTest/ClientTest.cs
+++ EasyPostTest/ClientTest.cs
@@ -48,6 +48,12 @@ namespace EasyPostTest {
         }
 
         [TestMethod]
+        public void TestRestClientWithOptions() {
+            Client client = new Client(new ClientConfiguration("someapikey", "http://apiBase.com"));
+            Assert.AreEqual(new Uri("http://apiBase.com"), client.client.BaseUrl);
+        }
+
+        [TestMethod]
         public void TestExecute() {
             Client client = new Client("http://echo.jsontest.com");
             Request request = new Request("key/value", Method.GET);
@@ -72,6 +78,17 @@ namespace EasyPostTest {
             List<String> parameters = client.PrepareRequest(request).Parameters.Select(parameter => parameter.ToString()).ToList();
             CollectionAssert.Contains(parameters, "user_agent=EasyPost/v2 CSharp/" + client.version);
             CollectionAssert.Contains(parameters, "authorization=Bearer " + Client.apiKey);
+            CollectionAssert.Contains(parameters, "content_type=application/x-www-form-urlencoded");
+        }
+
+        [TestMethod]
+        public void TestPrepareRequestWithOptions() {
+            Client client = new Client(new ClientConfiguration("someapikey", "http://foobar.com"));
+            Request request = new Request("resource");
+
+            List<String> parameters = client.PrepareRequest(request).Parameters.Select(parameter => parameter.ToString()).ToList();
+            CollectionAssert.Contains(parameters, "user_agent=EasyPost/v2 CSharp/" + client.version);
+            CollectionAssert.Contains(parameters, "authorization=Bearer someapikey");
             CollectionAssert.Contains(parameters, "content_type=application/x-www-form-urlencoded");
         }
     }
