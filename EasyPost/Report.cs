2d721b9e40dab7a410a399f48bc63be7108efca9
Andrew Tribone <tribone@easypost.com>
2016-12-13 09:58:59 -0800

    add Report object

---
+++ EasyPost/Report.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using RestSharp;
+
+namespace EasyPost {
+    public class Report : Resource {
+        public string id { get; set; }
+        public DateTime? created_at { get; set; }
+        public DateTime? updated_at { get; set; }
+        public DateTime? start_date { get; set; }
+        public DateTime? end_date { get; set; }
+        public string mode { get; set; }
+        public string status { get; set; }
+        public Boolean include_children { get; set; }
+        public string url { get; set; }
+        public DateTime? url_expires_at { get; set; }
+
+        /// <summary>
+        /// Retrieve a Report from its id and type.
+        /// </summary>
+        /// <param name="type">Type of report, e.g. shipment, tracker, payment_log, etc.</param>
+        /// <param name="id">String representing a report.</param>
+        /// <returns>EasyPost.Report instance.</returns>
+        public static Report Retrieve(string type, string id) {
+            Request request = new Request("reports/{type}/{id}");
+            request.AddUrlSegment("id", id);
+            request.AddUrlSegment("type", type);
+
+            return request.Execute<Report>();
+        }
+
+        /// <summary>
+        /// Create a Report.
+        /// </summary>
+        /// <param name="parameters">
+        /// Optional dictionary containing parameters to create the carrier account with. Valid pairs:
+        ///   * {"start_date", string} Date to start the report at.
+        ///   * {"end_date", string} Date to end the report at.
+        ///   * {"include_children", string} Whether or not to include child objects in the report.
+        /// All invalid keys will be ignored.
+        /// </param>
+        /// <returns>EasyPost.CarrierAccount instance.</returns>
+        public static Report Create(string type, Dictionary<string, object> parameters = null) {
+            parameters = parameters ?? new Dictionary<string, object>();
+
+            Request request = new Request("reports/{type}", Method.POST);
+            request.AddUrlSegment("type", type);
+            request.AddQueryString(parameters);
+
+            return request.Execute<Report>();
+        }
+    }
+}
---
+++ EasyPostTest/ReportTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using EasyPost;
+
+namespace EasyPostTest {
+    [TestClass]
+    public class ReportTest {
+        [TestInitialize]
+        public void Initialize() {
+            ClientManager.SetCurrent("cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi");
+        }
+
+        [TestMethod]
+        public void TestCreateAndRetrieve() {
+            Dictionary<string, object> parameters = new Dictionary<string, object>() {
+                { "include_children", true },
+                { "end_date", "2016-06-01" }
+            };
+            Report report = Report.Create("shipment", parameters);
+            Assert.IsNotNull(report.id);
+            Assert.IsTrue(report.include_children);
+
+            Report retrieved = Report.Retrieve("shipment", report.id);
+            Assert.AreEqual(report.id, retrieved.id);
+        }
+    }
+}
