f2e4a83b4209c3ccf3bbd4e705f94ca9e3a6de3f
Max Waldt <max@easypost.com>
2018-01-09 12:29:27 -0500

    Adding print_custom array and peel_and_return shipment options (#107)

--- EasyPost/Options.cs
+++ EasyPost/Options.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EasyPost {
     public class Options : Resource {
@@ -53,6 +54,8 @@ namespace EasyPost {
         public string print_custom_1_code { get; set; }
         public string print_custom_2_code { get; set; }
         public string print_custom_3_code { get; set; }
+        public List<Dictionary<string, object>> print_custom { get; set;}
+        public bool peel_and_return { get; set; }
         public bool? print_custom_1_barcode { get; set; }
         public bool? print_custom_2_barcode { get; set; }
         public bool? print_custom_3_barcode { get; set; }
--- EasyPost/Properties/AssemblyInfo.cs
+++ EasyPost/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EasyPost")]
@@ -17,8 +17,8 @@ using System.Runtime.InteropServices;
 // Make "private" methods testable.
 [assembly: InternalsVisibleTo("EasyPostTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd0773e2410e7775bdb1dcdf4f18bde613db939e4687094c4e35da5c25e029fce5a0598f03ace975f005b002f1e56d42bc00e2c54aa41cc416da712620013b3a35ab016f1ad5382e4742cc725434129b0acb8c8f7c900609d2c65bf00998a9b0c8576299f0efa0d8d8b742b035cdcaf916ea12234fec67273bd4753677a3ccae")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -28,13 +28,13 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.3.1.3")]
-[assembly: AssemblyInformationalVersion("2.3.1.3")]
+[assembly: AssemblyFileVersion("2.3.1.4")]
+[assembly: AssemblyInformationalVersion("2.3.1.4")]
