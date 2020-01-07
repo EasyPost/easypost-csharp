2917623e0ee54831e2391b61b63fb75a4384f07e
Andrew Tribone <tribone@easypost.com>
2020-01-07 07:49:28 -0800

    bump version to 2.5.1.3

--- EasyPost/Properties/AssemblyInfo.cs
+++ EasyPost/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.5.1.2")]
-[assembly: AssemblyInformationalVersion("2.5.1.2")]
+[assembly: AssemblyFileVersion("2.5.1.3")]
+[assembly: AssemblyInformationalVersion("2.5.1.3")]
