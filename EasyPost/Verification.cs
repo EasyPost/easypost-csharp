cd2e8e535be14784c7450260dc884d2c142341cd
rbellow <50749020+rbellow@users.noreply.github.com>
2020-02-13 20:30:22 -0600

    Update Verification.cs

--- EasyPost/Verification.cs
+++ EasyPost/Verification.cs
@@ -4,5 +4,6 @@ namespace EasyPost {
     public class Verification : Resource {
         public bool success { get; set; }
         public List<Error> errors { get; set; }
+        public List<VerificationDetails> details { get; set; }
     }
 }
