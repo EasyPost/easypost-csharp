20129c19a003aeddfb2bfdb97719ed8412563bf8
Miguel Martorell <1791215+hellbinder@users.noreply.github.com>
2021-08-16 12:44:03 -0400

    Add netcore31 (#172)
    * Added VersionInfo.cs to manage assembly version under one file
    
    * Added netcoreapp 3.1 project
    
    * EasyPost project will have have the pfx file while the other projects will hold a reference to it.
    
    * Clean up project files
    
    Remove double extra lines in project files, move comments from assmebly info to version info and fix VersionFile reference in EasyPost project
    
    * VersionInfo include missed commented line

--- EasyPost.Net35/Properties/AssemblyInfo.cs
+++ EasyPost.Net35/Properties/AssemblyInfo.cs
@@ -21,17 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f5090d7a-c84e-4f9e-a60d-8148c5db6aa2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.5.1.3")]
-[assembly: AssemblyInformationalVersion("2.5.1.3")]
--- EasyPost/Properties/AssemblyInfo.cs
+++ EasyPost/Properties/AssemblyInfo.cs
@@ -29,16 +29,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("754ac1cb-281c-4d4c-b242-5fb436ee1832")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.5.1.3")]
-[assembly: AssemblyInformationalVersion("2.5.1.3")]
