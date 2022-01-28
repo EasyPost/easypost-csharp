7cf53dc0f50621c22e560bf004e0ff1c5aa829c0
Nate Harris <nharris@easypost.com>
2022-01-28 13:10:41 -0700

    - Fix generic "var" in commented-out tests

--- EasyPost.Tests/ClientManagerTest.cs
+++ EasyPost.Tests/ClientManagerTest.cs
@@ -13,15 +13,15 @@
 //         [TestMethod]
 //         public void TestApiKey() {
 //             ClientManager.SetCurrent("apiKey");
-//             var client = ClientManager.Build();
+//             Client client = ClientManager.Build();
 //             Assert.AreEqual("apiKey", client.configuration.ApiKey);
 //         }
 
 //         [TestMethod]
 //         public void TestApiKeyForUniqueObjects() {
 //             ClientManager.SetCurrent("apiKey");
-//             var client1 = ClientManager.Build();
-//             var client2 = ClientManager.Build();
+//             Client client1 = ClientManager.Build();
+//             Client client2 = ClientManager.Build();
 
 //             Assert.AreNotSame(client1, client2);
 //         }
@@ -29,15 +29,15 @@
 //         [TestMethod]
 //         public void TestFactoryDelegate() {
 //             ClientManager.SetCurrent(() => new Client(new ClientConfiguration("apiKey")));
-//             var client = ClientManager.Build();
+//             Client client = ClientManager.Build();
 //             Assert.AreEqual("apiKey", client.configuration.ApiKey);
 //         }
 
 //         [TestMethod]
 //         public void TestFactoryDelegateForUniqueObjects() {
 //             ClientManager.SetCurrent(() => new Client(new ClientConfiguration("apiKey")));
-//             var client1 = ClientManager.Build();
-//             var client2 = ClientManager.Build();
+//             Client client1 = ClientManager.Build();
+//             Client client2 = ClientManager.Build();
 
 //             Assert.AreNotSame(client1, client2);
 //         }
