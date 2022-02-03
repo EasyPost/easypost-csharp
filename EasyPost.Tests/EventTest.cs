8fe504201b7d0e31bdf664fcd2e039e5808b932b
Nate Harris <nwithan8@users.noreply.github.com>
2022-02-03 15:07:23 -0700

    Add support for retrieving all events (#201)
    * - Added Event.All() function to retrieve all events, returns a `EventCollection` object
    - Added `EventCollection` class to all projects/versions
    - Added `EventTest.TestRetrieveAll()` test
    - Updated CHANGELOG
    
    * - Clarify docstring for `Event.All()` function
    
    * - Check each event when testing `Event.All()`

--- EasyPost.Tests/EventTest.cs
+++ EasyPost.Tests/EventTest.cs
@@ -18,5 +18,17 @@ namespace EasyPost.Tests
             // Events are archived after some time. Lets at least make sure we get a 404.
             Event e = Event.Retrieve("evt_d0000c1a9c6c4614949af6931ea9fac8");
         }
+
+        [TestMethod]
+        public void TestRetrieveAll()
+        {
+            EventCollection eventCollection = Event.All();
+            Assert.IsNotNull(eventCollection);
+            foreach (var tEvent in eventCollection.events)
+            {
+                Assert.IsNotNull(tEvent.id);
+                Assert.AreEqual(tEvent.id.Substring(0, 4), "evt_");
+            }
+        }
     }
 }
