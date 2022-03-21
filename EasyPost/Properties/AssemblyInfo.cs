17ae1c2e7f7dab18144c13c992d365649e6985d2
Nate Harris <nharris@easypost.com>
2022-03-21 14:35:22 -0600

    - Fix assembly copyright

--- EasyPost/Properties/AssemblyInfo.cs
+++ EasyPost/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EasyPost")]
 [assembly: AssemblyProduct("EasyPost")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
