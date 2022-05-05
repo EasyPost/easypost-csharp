e2e9d5b4ee647879e3c95b3f9729dd7c90770ab1
Nate Harris <nharris@easypost.com>
2022-05-05 15:44:19 -0600

    - Switch to DefaultStrict VCR match mode - Modify VCR to only record if cassette does not exist, otherwise attempt replay (no auto mode) - Use EasyVCR 0.2.0 to fix body matching issue

--- EasyPost.Tests/TestUtils.cs
+++ EasyPost.Tests/TestUtils.cs
@@ -11,9 +11,10 @@ namespace EasyPost.Tests
 
         private const string CassettesFolder = "cassettes";
 
-        private static string GetSourceFileDirectory([CallerFilePath] string sourceFilePath = "")
+        public enum ApiKey
         {
-            return Path.GetDirectoryName(sourceFilePath);
+            Test,
+            Production
         }
 
         private static string GetApiKey(ApiKey apiKey)
@@ -34,31 +35,28 @@ namespace EasyPost.Tests
             return Environment.GetEnvironmentVariable(keyName) ?? ApiKeyFailedToPull; // if can't pull from environment, will use a fake key. Won't matter on replay.
         }
 
-        public enum ApiKey
+        private static string GetSourceFileDirectory([CallerFilePath] string sourceFilePath = "")
         {
-            Test,
-            Production
+            return Path.GetDirectoryName(sourceFilePath);
         }
 
         public class VCR
         {
-            private readonly EasyVCR.VCR _vcr;
-
             private readonly string _apiKey;
 
             private readonly string _testCassettesFolder;
+            private readonly EasyVCR.VCR _vcr;
 
             public VCR(string testCassettesFolder = null, ApiKey apiKey = ApiKey.Test)
             {
                 AdvancedSettings advancedSettings = new AdvancedSettings
                 {
-                    MatchRules = MatchRules.Default,
+                    MatchRules = MatchRules.DefaultStrict,
                     Censors = Censors.DefaultSensitive,
                     SimulateDelay = false,
                     ManualDelay = 0,
                 };
                 _vcr = new EasyVCR.VCR(advancedSettings);
-                _vcr.RecordIfNeeded(); // always in auto mode
 
                 _apiKey = GetApiKey(apiKey);
 
@@ -100,8 +98,21 @@ namespace EasyPost.Tests
                 // set up cassette
                 Cassette cassette = new Cassette(_testCassettesFolder, cassetteName, new CassetteOrder.Alphabetical());
 
+                string filePath = Path.Combine(_testCassettesFolder, cassetteName + ".json");
+
                 // add cassette to vcr
                 _vcr.Insert(cassette);
+
+                if (!File.Exists(filePath))
+                {
+                    // if cassette doesn't exist, switch to record mode
+                    _vcr.Record();
+                }
+                else
+                {
+                    // if cassette exists, switch to replay mode
+                    _vcr.Replay();
+                }
 
                 // set up EasyPost client
                 ClientManager.SetCurrent(() => new Client(new ClientConfiguration(apiKey), _vcr.Client));
