8caf5cfb345da0167f3d358d9618590979f8401d
Nate Harris <nharris@easypost.com>
2022-06-25 22:30:26 -0600

    - Remove unit test for sample

--- EasyPost.Tests/Sample.cs
+++ EasyPost.Tests/Sample.cs
@@ -1,13 +1,11 @@
 using System.Threading.Tasks;
 using EasyPost.Clients;
 using EasyPost.Services.Beta;
-using Xunit;
 
 namespace EasyPost.Tests
 {
     public class Sample
     {
-        [Fact]
         public async Task Main()
         {
             Client client = new Client("my_api_key", ApiVersion.Latest);
