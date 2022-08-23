e01cdf9a10549e1068a910270237e819033e02f8
Nate Harris <nharris@easypost.com>
2022-08-23 16:38:43 -0600

    - WIP: Thread safety unit test

--- EasyPost.Tests/ClientTest.cs
+++ EasyPost.Tests/ClientTest.cs
@@ -7,6 +7,15 @@ namespace EasyPost.Tests
         private const string FakeApikey = "fake_api_key";
 
         [Fact]
+        public void TestThreadSafety()
+        {
+            Client client1 = new Client(TestUtils.GetApiKey(TestUtils.ApiKey.Test));
+            Client client2 = new Client(TestUtils.GetApiKey(TestUtils.ApiKey.Production));
+
+            // TODO: How do we prove thread safety?
+        }
+
+        [Fact]
         public void TestTimeout()
         {
             // ReSharper disable once UseObjectOrCollectionInitializer
