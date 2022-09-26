165db982c989fd23cb86352219e0bb16a32f8436
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-09-26 16:19:25 -0600

    fix: AssemblyVersion must be numerical only

--- EasyPost/Properties/VersionInfo.cs
+++ EasyPost/Properties/VersionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 // Version information for an assembly must follow semantic versioning
-[assembly: AssemblyVersion("4.0.0-rc1")]
-[assembly: AssemblyFileVersion("4.0.0-rc1")]
-[assembly: AssemblyInformationalVersion("4.0.0-rc1")]
+[assembly: AssemblyVersion("4.0.0.1")]
+[assembly: AssemblyFileVersion("4.0.0.1")]
+[assembly: AssemblyInformationalVersion("4.0.0.1")]
