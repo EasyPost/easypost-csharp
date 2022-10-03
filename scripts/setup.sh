007029c3f82dcf04048fde28dba9c5cc853732f8
nharris <nharris@easypost.com>
2022-10-03 14:31:00 -0600

    - Address feedback

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,17 @@
 # This script is used to download and install the required .NET versions
 
 # .NET versions we want to install
-declare -a NetVersions=("Current" "6.0" "5.0" "3.1" )
+declare -a NetVersions=("Current" "6.0" "5.0" "3.1")
 
 # Download dotnet-install.sh
 echo "Downloading dotnet-install.sh script..."
 curl -sSL https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
- 
+
 # Install each .NET version
 echo "Installing .NET versions..."
 for version in ${NetVersions[@]}; do
-   echo "Installing .NET $version..."
-   sudo bash ./dotnet-install.sh -c "$version"
+    echo "Installing .NET $version..."
+    sudo bash ./dotnet-install.sh -c "$version"
 done
 
 # Remove dotnet-install.sh
