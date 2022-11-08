7559c6eea2d43b490bc90c281ade0b3f398549a0
Nate Harris <nwithan8@users.noreply.github.com>
2022-11-07 23:36:01 -0700

    Enforce code coverage threshold (#368)
    - New CI step (and Makefile local option) to check code coverage
    - Enforce 90% line coverage for unit tests
    - Run code coverage CI steps on Linux (since using Bash scripts)
    - Only run code coverage check on net6.0 to avoid issues with missing dotnet versions on Linux machine

---
+++ check_coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+THRESHOLD=90
+THRESHOLD_TYPE=line
+
+# Analyze test coverage
+test_folder="EasyPost.Tests"
+cd "$test_folder" || exit
+
+# Coverlet as a global tool does not report coverage properly (https://github.com/coverlet-coverage/coverlet/blob/2b8a4565b101ca70c3eaa9b5228c449dd3b81ad1/Documentation/GlobalTool.md)
+# Instead, use the coverlet.msbuild package
+# https://codeburst.io/code-coverage-in-net-core-projects-c3d6536fd7d7
+# This will fail to run on machines without MSBuild installed
+
+# If the coverage is below the threshold, exit with a non-zero exit code
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:Threshold=$THRESHOLD /p:ThresholdType=$THRESHOLD_TYPE /p:TargetFramework=net6.0
