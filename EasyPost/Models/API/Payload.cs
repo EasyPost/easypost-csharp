b4d0d3096ac50c1f7de6696a13f911552402bc2e
nharris <nharris@easypost.com>
2023-01-13 18:23:47 -0700

    - Fix payload schema

--- EasyPost/Models/API/Payload.cs
+++ EasyPost/Models/API/Payload.cs
@@ -1,8 +1,27 @@
+using System.Collections.Generic;
+using EasyPost._base;
+using Newtonsoft.Json;
+
 namespace EasyPost.Models.API
 {
-    public class Payload : Event // A payload has the same schema as an Event
+    public class Payload : EasyPostObject
     {
         #region JSON Properties
+
+        [JsonProperty("request_url")]
+        public string? RequestUrl { get; set; }
+        [JsonProperty("request_headers")]
+        public Dictionary<string, string>? RequestHeaders { get; set; }
+        [JsonProperty("request_body")]
+        public Dictionary<string, object>? RequestBody { get; set; }
+        [JsonProperty("response_headers")]
+        public Dictionary<string, string>? ResponseHeaders { get; set; }
+        [JsonProperty("response_body")]
+        public string? ResponseBody { get; set; }
+        [JsonProperty("response_code")]
+        public int? ResponseCode { get; set; }
+        [JsonProperty("total_time")]
+        public int? TotalTime { get; set; }
 
         #endregion
 
