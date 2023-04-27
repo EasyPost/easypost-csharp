8b2fd9dce4774cf06605eb10f14b1f8d446e09b0
nwithan8 <nwithan8@users.noreply.github.com>
2023-04-27 16:57:29 -0600

    - Fix F# test

--- EasyPost.Tests.FSharp/FSharpCompileTest.fs
+++ EasyPost.Tests.FSharp/FSharpCompileTest.fs
@@ -9,6 +9,6 @@ open Xunit
 type FSharpCompileTest() =
     [<Fact>]
     member this.TestCompile() =
-        let client = new Client("fake_api_key")
+        let client = new Client(new ClientConfiguration("fake_api_key"))
         // The assert doesn't really do anything, but as long as this test can run, then the code is compiling correctly.
         Assert.NotNull(client)
