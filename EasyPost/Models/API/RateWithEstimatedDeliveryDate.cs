bd01dd878d62db25500a3c611319373bca396f5e
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-01 13:32:00 -0600

    - Add new models for smart rates with estimated delivery dates - Change base class for TimeInTransit since it is ephemeral

---
+++ EasyPost/Models/API/RateWithEstimatedDeliveryDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EasyPost._base;
+using Newtonsoft.Json;
+
+namespace EasyPost.Models.API
+{
+    public class RateWithEstimatedDeliveryDate
+    {
+        #region JSON Properties
+
+        [JsonProperty("rate")]
+        public Rate? Rate { get; set; }
+        [JsonProperty("easypost_time_in_transit_data")]
+        public TimeInTransitDetails? EasyPostTimeInTransitData { get; set; }
+
+        #endregion
+
+        internal RateWithEstimatedDeliveryDate()
+        {
+        }
+    }
+
+    public class TimeInTransitDetails
+    {
+        #region JSON Properties
+
+        [JsonProperty("days_in_transit")]
+        public TimeInTransit? DaysInTransit { get; set; }
+        [JsonProperty("easypost_time_in_transit_data")]
+        public string? EasyPostEstimatedDeliveryDate { get; set; }
+        [JsonProperty("planned_ship_date")]
+        public DateTime? PlannedShipDate { get; set; }
+
+        #endregion
+        
+        internal TimeInTransitDetails()
+        {
+        }
+    }
+}
