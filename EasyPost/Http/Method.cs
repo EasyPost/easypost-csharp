6dfdc3156865d38d56384569bfce14314447181f
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-03 11:01:02 -0600

    - Add docstrings to all base classes (clients, services, objects) - Add docstrings to HTTP method enums

--- EasyPost/Http/Method.cs
+++ EasyPost/Http/Method.cs
@@ -7,29 +7,40 @@ namespace EasyPost.Http
     /// </summary>
     public class Method
     {
+        /// <summary>
+        ///     The <see cref="HttpMethod"/> associated with this enum.
+        /// </summary>
         internal HttpMethod HttpMethod { get; }
 
         /// <summary>
         ///     HTTP GET method.
         /// </summary>
         public static readonly Method Get = new Method(HttpMethod.Get);
+
         /// <summary>
         ///     HTTP POST method.
         /// </summary>
         public static readonly Method Post = new Method(HttpMethod.Post);
+
         /// <summary>
         ///     HTTP PUT method.
         /// </summary>
         public static readonly Method Put = new Method(HttpMethod.Put);
+
         /// <summary>
         ///     HTTP DELETE method.
         /// </summary>
         public static readonly Method Delete = new Method(HttpMethod.Delete);
+
         /// <summary>
         ///     HTTP PATCH method.
         /// </summary>
         public static readonly Method Patch = new Method(new HttpMethod("PATCH"));
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Method"/> class.
+        /// </summary>
+        /// <param name="httpMethod">The <see cref="HttpMethod"/> to associate with this enum.</param>
         private Method(HttpMethod httpMethod)
         {
             HttpMethod = httpMethod;
--- EasyPost/_base/EasyPostObject.cs
+++ EasyPost/_base/EasyPostObject.cs
@@ -17,25 +17,47 @@ namespace EasyPost._base
     {
         #region JSON Properties
 
+        /// <summary>
+        ///     The date and time this object was created.
+        /// </summary>
         [JsonProperty("created_at")]
         public DateTime? CreatedAt { get; internal set; }
+
+        /// <summary>
+        ///     The EasyPost ID for this object.
+        /// </summary>
         [JsonProperty("id")]
         public string? Id { get; internal set; }
+
+        /// <summary>
+        ///     The date and time this object was last updated.
+        /// </summary>
         [JsonProperty("updated_at")]
         public DateTime? UpdatedAt { get; internal set; }
 
         #endregion
 
+        /// <summary>
+        ///     The prefix of the EasyPost ID for this object.
+        /// </summary>
         internal string? Prefix => Id?.Split('_').First();
 
+        /// <summary>
+        ///     Gets this object as a JSON object (dictionary).
+        /// </summary>
+        /// <returns>A dictionary representation of this object's properties.</returns>
         internal Dictionary<string, object> AsDictionary() => JsonConvert.DeserializeObject<Dictionary<string, object>>(AsJson())!;
 
+        /// <inheritdoc />
         public override bool Equals(object? obj) => GetType() == obj?.GetType() && GetHashCode() == ((EasyPostObject)obj).GetHashCode();
 
+        /// <inheritdoc />
+        // ReSharper disable once NonReadonlyMemberInGetHashCode
 #pragma warning disable CA1307
-        public override int GetHashCode() => AsJson().GetHashCode() ^ GetType().GetHashCode();
+        public override int GetHashCode() => base.GetHashCode() ^ Id!.GetHashCode();
 #pragma warning restore CA1307
 
+        /// <inheritdoc cref="EphemeralEasyPostObject.operator =="/>
         public static bool operator ==(EasyPostObject? one, object? two)
         {
             if (one is null && two is null)
@@ -53,6 +75,7 @@ namespace EasyPost._base
             return one.Equals(two);
         }
 
+        /// <inheritdoc cref="EphemeralEasyPostObject.operator !="/>
         public static bool operator !=(EasyPostObject? one, object? two) => !(one == two);
     }
 }
