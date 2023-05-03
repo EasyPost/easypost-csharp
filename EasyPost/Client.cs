7d144acbb4a59d01dde93cb75d805c3efc4165c8
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-03 17:56:26 -0600

    - Docstrings for clients and constants

--- EasyPost/Client.cs
+++ EasyPost/Client.cs
@@ -5,62 +5,137 @@ using EasyPost.Services;
 
 namespace EasyPost
 {
+    /// <summary>
+    ///     Access EasyPost API functionality.
+    /// </summary>
     public class Client : EasyPostClient
     {
+        /// <summary>
+        ///     Access Address-related functionality.
+        /// </summary>
         public AddressService Address { get; }
 
+        /// <summary>
+        ///     Access API Key-related functionality.
+        /// </summary>
         public ApiKeyService ApiKey { get; } // TODO: Recommend renaming. Because ApiKey is used as the name of the API key service, you have to access the actual configured API key via ApiKeyInUse
 
+        /// <summary>
+        ///     Access Batch-related functionality.
+        /// </summary>
         public BatchService Batch { get; }
 
         // ReSharper disable once MemberCanBePrivate.Global
         // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        /// <summary>
+        ///     Access beta functionality.
+        /// </summary>
         public BetaClient Beta { get; }
-
+        
+        /// <summary>
+        ///     Access Billing-related functionality.
+        /// </summary>
         public BillingService Billing { get; }
 
+        /// <summary>
+        ///     Access Carrier Account-related functionality.
+        /// </summary>
         public CarrierAccountService CarrierAccount { get; }
 
+        /// <summary>
+        ///     Access Carrier Type-related functionality.
+        /// </summary>
         public CarrierTypeService CarrierType { get; }
 
+        /// <summary>
+        ///     Access Customs Info-related functionality.
+        /// </summary>
         public CustomsInfoService CustomsInfo { get; }
 
+        /// <summary>
+        ///     Access Customs Item-related functionality.
+        /// </summary>
         public CustomsItemService CustomsItem { get; }
 
+        /// <summary>
+        ///     Access EndShipper-related functionality.
+        /// </summary>
         public EndShipperService EndShipper { get; }
 
+        /// <summary>
+        ///     Access Event-related functionality.
+        /// </summary>
         public EventService Event { get; }
 
+        /// <summary>
+        ///     Access Insurance-related functionality.
+        /// </summary>
         public InsuranceService Insurance { get; }
 
+        /// <summary>
+        ///     Access Order-related functionality.
+        /// </summary>
         public OrderService Order { get; }
 
+        /// <summary>
+        ///     Access Parcel-related functionality.
+        /// </summary>
         public ParcelService Parcel { get; }
 
+        /// <summary>
+        ///     Access Pickup-related functionality.
+        /// </summary>
         public PickupService Pickup { get; }
 
+        /// <summary>
+        ///     Access Rate-related functionality.
+        /// </summary>
         public RateService Rate { get; }
 
+        /// <summary>
+        ///     Access Referral Customer-related functionality.
+        /// </summary>
         public ReferralCustomerService ReferralCustomer { get; }
 
+        /// <summary>
+        ///     Access Refund-related functionality.
+        /// </summary>
         public RefundService Refund { get; }
 
+        /// <summary>
+        ///     Access Report-related functionality.
+        /// </summary>
         public ReportService Report { get; }
 
+        /// <summary>
+        ///     Access Scan Form-related functionality.
+        /// </summary>
         public ScanFormService ScanForm { get; }
 
+        /// <summary>
+        ///     Access Shipment-related functionality.
+        /// </summary>
         public ShipmentService Shipment { get; }
 
+        /// <summary>
+        ///     Access Tracker-related functionality.
+        /// </summary>
         public TrackerService Tracker { get; }
 
+        /// <summary>
+        ///     Access User-related functionality.
+        /// </summary>
         public UserService User { get; }
 
+        /// <summary>
+        ///     Access Webhook-related functionality.
+        /// </summary>
         public WebhookService Webhook { get; }
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="Client"/> class.
         /// </summary>
-        /// <param name="configuration">Configuration for this client.</param>
+        /// <param name="configuration"><see cref="ClientConfiguration"/> for this client.</param>
 #pragma warning disable IDE0021 // Ignoring since more properties will be added during construction in the future.
         public Client(ClientConfiguration configuration)
             : base(configuration)
@@ -95,6 +170,7 @@ namespace EasyPost
             Beta = new BetaClient(configuration);
         }
 
+        /// <inheritdoc cref="EasyPostClient.Dispose(bool)"/>
         protected override void Dispose(bool disposing)
         {
             // ref: https://dzone.com/articles/when-and-how-to-use-dispose-and-finalize-in-c
