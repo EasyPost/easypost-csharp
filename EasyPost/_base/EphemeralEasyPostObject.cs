b294bcde909622007e5fb60354fd2884fe5956a6
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-08 12:12:58 -0600

    - Static analysis

--- EasyPost/_base/EphemeralEasyPostObject.cs
+++ EasyPost/_base/EphemeralEasyPostObject.cs
@@ -29,8 +29,10 @@ namespace EasyPost._base
         /// <inheritdoc />
         public override bool Equals(object? obj) => GetType() == obj?.GetType() && GetHashCode() == ((EphemeralEasyPostObject)obj).GetHashCode();
 
+#pragma warning disable CA1307 // Specify StringComparison
         /// <inheritdoc />
         public override int GetHashCode() => AsJson().GetHashCode() ^ GetType().GetHashCode();
+#pragma warning restore CA1307 // Specify StringComparison
 
         /// <summary>
         ///     Compare two objects for equality.
