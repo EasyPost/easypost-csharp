6d2ef482fe2f315729c1548b7e44099415fd1a4d
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-09 12:20:10 -0600

    - Address feedback

--- EasyPost/_base/EphemeralEasyPostObject.cs
+++ EasyPost/_base/EphemeralEasyPostObject.cs
@@ -28,8 +28,7 @@ namespace EasyPost._base
 
         /// <inheritdoc />
         public override bool Equals(object? obj) => GetType() == obj?.GetType() && GetHashCode() == ((EphemeralEasyPostObject)obj).GetHashCode();
-
-#pragma warning disable CA1307 // Specify StringComparison
+        
         /// <inheritdoc />
 #pragma warning disable CA1307 // Specify StringComparison
         public override int GetHashCode() => AsJson().GetHashCode() ^ GetType().GetHashCode();
