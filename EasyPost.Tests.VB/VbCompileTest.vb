5b22923b7a67f6ff18b8c4135e93eb23075c4c90
Nate Harris <nwithan8@users.noreply.github.com>
2023-05-10 15:44:23 -0600

    - Target NET 7.0 for VB and F# compile tests (#465)

--- EasyPost.Tests.VB/VbCompileTest.vb
+++ EasyPost.Tests.VB/VbCompileTest.vb
@@ -1,5 +1,5 @@
 'This test checks that EasyPost C# code can be used in Visual Basic.
-'This test project is running on .NET 6.0, although a success here should mean a success in all versions of .NET.
+'This test project is running on .NET 7.0, although a success here should mean a success in all versions of .NET.
 Imports Xunit
 
 Public Class VbCompileTest
