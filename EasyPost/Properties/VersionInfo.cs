be7e02e0da43b364cbe6bbfbfc6628614f62113c
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-15 15:37:55 -0600

    - Bump version to 4.6.2

--- EasyPost/Properties/VersionInfo.cs
+++ EasyPost/Properties/VersionInfo.cs
@@ -2,6 +2,6 @@
 
 // Version information for an assembly must follow semantic versioning
 // When releasing a release candidate, append a 4th digit being the number of the release candidate
-[assembly: AssemblyVersion("4.6.1")]
-[assembly: AssemblyFileVersion("4.6.1")]
-[assembly: AssemblyInformationalVersion("4.6.1")]
+[assembly: AssemblyVersion("4.6.2")]
+[assembly: AssemblyFileVersion("4.6.2")]
+[assembly: AssemblyInformationalVersion("4.6.2")]
