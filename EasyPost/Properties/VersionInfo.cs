10a5bbc65073024e6e1a8a931fa05feae2f0bb92
nwithan8 <nwithan8@users.noreply.github.com>
2023-07-12 10:31:54 -0600

    - Prep for v5.2.0 release

--- EasyPost/Properties/VersionInfo.cs
+++ EasyPost/Properties/VersionInfo.cs
@@ -2,6 +2,6 @@
 
 // Version information for an assembly must follow semantic versioning
 // When releasing a release candidate, append a 4th digit being the number of the release candidate
-[assembly: AssemblyVersion("5.1.1")]
-[assembly: AssemblyFileVersion("5.1.1")]
-[assembly: AssemblyInformationalVersion("5.1.1")]
+[assembly: AssemblyVersion("5.2.0")]
+[assembly: AssemblyFileVersion("5.2.0")]
+[assembly: AssemblyInformationalVersion("5.2.0")]
