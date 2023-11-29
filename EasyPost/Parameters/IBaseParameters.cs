f32b21b447f069cfaa93da26e0abdacf0c0a3fe1
Nate Harris <nwithan8@users.noreply.github.com>
2023-11-29 14:59:03 -0700

    [chore] Add missing docstrings (#522)

--- EasyPost/Parameters/IBaseParameters.cs
+++ EasyPost/Parameters/IBaseParameters.cs
@@ -12,10 +12,22 @@ using EasyPost.Utilities.Internal.Extensions;
 
 namespace EasyPost.Parameters
 {
+    /// <summary>
+    ///     Base interface for all EasyPost API parameters.
+    /// </summary>
     public interface IBaseParameters
     {
+        /// <summary>
+        ///     Convert this object to a <see cref="Dictionary{TKey,TValue}"/>.
+        /// </summary>
+        /// <returns></returns>
         public Dictionary<string, object> ToDictionary();
 
+        /// <summary>
+        ///     Convert this object to a sub-<see cref="Dictionary{TKey,TValue}"/> based on the parent parameter object type.
+        /// </summary>
+        /// <param name="parentParameterObjectType"></param>
+        /// <returns></returns>
         public Dictionary<string, object> ToSubDictionary(Type parentParameterObjectType);
     }
 }
