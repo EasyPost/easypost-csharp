421c418656cff429976384d72c9682cbcc85a5e8
nwithan8 <nwithan8@users.noreply.github.com>
2023-12-21 17:16:28 -0700

    - Re-record cassettes as needed

--- EasyPost.Tests/Fixture.cs
+++ EasyPost.Tests/Fixture.cs
@@ -54,7 +54,7 @@ namespace EasyPost.Tests._Utilities
             {
                 Dictionary<string, object> fixture = GetFixtureStructure().Pickups.Basic;
 
-                const string pickupDate = "2023-12-01";
+                const string pickupDate = "2023-12-25";
 
                 fixture!.AddOrUpdate("min_datetime", pickupDate);
                 fixture!.AddOrUpdate("max_datetime", pickupDate);
@@ -89,7 +89,7 @@ namespace EasyPost.Tests._Utilities
 
         internal static string PickupService => GetFixtureStructure().ServiceNames.Usps.PickupService;
 
-        internal static string PlannedShipDate => "2023-12-01";
+        internal static string PlannedShipDate => "2023-12-25";
 
         internal static Dictionary<string, object> ReferralCustomer => GetFixtureStructure().Users.Referral;
 
