9a242160dde0f1e3c2ab95f367649aadd1847565
Nate Harris <nwithan8@users.noreply.github.com>
2024-03-17 19:23:28 -0600

    - Fix carrier account parameter serialization for stateless rates - Update, sync related unit tests - Re-record relevant cassettes

--- EasyPost.Tests/ServicesTests/WithParameters/Beta/RateServiceTest.cs
+++ EasyPost.Tests/ServicesTests/WithParameters/Beta/RateServiceTest.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using EasyPost.Models.API.Beta;
 using EasyPost.Tests._Utilities;
 using EasyPost.Tests._Utilities.Attributes;
+using EasyPost.Utilities.Internal.Attributes;
 using Xunit;
 
 namespace EasyPost.Tests.ServicesTests.WithParameters.Beta
@@ -17,13 +18,12 @@ namespace EasyPost.Tests.ServicesTests.WithParameters.Beta
 
         #region Test CRUD Operations
 
-        #endregion
-
         [Fact]
+        [CrudOperations.Read]
         [Testing.Function]
-        public async Task TestGetRates()
+        public async Task TestRetrieveStatelessRates()
         {
-            UseVCR("get_rates");
+            UseVCR("retrieve_stateless_rates");
 
             Dictionary<string, object> data = Fixtures.BasicShipment;
 
@@ -34,6 +34,33 @@ namespace EasyPost.Tests.ServicesTests.WithParameters.Beta
             Assert.NotNull(rates);
             Assert.NotEmpty(rates);
         }
+
+        [Fact]
+        [CrudOperations.Read]
+        [Testing.Function]
+        public async Task TestRetrieveStatelessRatesWithCarrierAccount()
+        {
+            UseVCR("retrieve_stateless_rates_with_carrier_account");
+
+            Dictionary<string, object> shipmentData = Fixtures.BasicShipment;
+
+            Parameters.Beta.Rate.Retrieve parameters = Fixtures.Parameters.Rates.RetrieveBeta(shipmentData);
+
+            parameters.CarrierAccounts = new List<Models.API.CarrierAccount>
+            {
+                new Models.API.CarrierAccount
+                {
+                    Id = Fixtures.UspsCarrierAccountId,
+                }
+            };
+
+            List<StatelessRate> rates = await Client.Beta.Rate.RetrieveStatelessRates(parameters);
+
+            Assert.NotNull(rates);
+            Assert.NotEmpty(rates);
+        }
+
+        #endregion
 
         #endregion
     }
