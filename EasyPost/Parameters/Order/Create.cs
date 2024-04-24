9e0ac526375688bb5434e1c14194683e8a8fe632
Nate Harris <nwithan8@users.noreply.github.com>
2024-04-24 10:37:01 -0600

    - Port missing create shipment parameters to create order

--- EasyPost/Parameters/Order/Create.cs
+++ EasyPost/Parameters/Order/Create.cs
@@ -13,11 +13,24 @@ namespace EasyPost.Parameters.Order
         #region Request Parameters
 
         /// <summary>
+        ///     The <see cref="Address"/> (or <see cref="Address.Create"/> parameters) of the buyer for the new <see cref="Models.API.Order"/>.
+        ///     Defaults to <see cref="ToAddress"/> if not provided.
+        /// </summary>
+        [TopLevelRequestParameter(Necessity.Optional, "order", "buyer_address")]
+        public IAddressParameter? BuyerAddress { get; set; }
+
+        /// <summary>
         ///     List of <see cref="Models.API.CarrierAccount"/>s to use to create the new <see cref="Models.API.Order"/>.
         ///     The provided <see cref="Models.API.CarrierAccount"/>s must exist prior to making the API call.
         /// </summary>
         [TopLevelRequestParameter(Necessity.Optional, "order", "carrier_accounts")]
         public List<Models.API.CarrierAccount>? CarrierAccounts { get; set; }
+
+        /// <summary>
+        ///     <see cref="Models.API.CustomsInfo"/> (or a <see cref="Parameters.CustomsInfo.Create"/> parameter set) for the new <see cref="Models.API.Order"/>.
+        /// </summary>
+        [TopLevelRequestParameter(Necessity.Optional, "order", "customs_info")]
+        public ICustomsInfoParameter? CustomsInfo { get; set; }
 
         /// <summary>
         ///     The origin <see cref="Models.API.Address"/> (or <see cref="Address.Create"/> parameters) for the new <see cref="Models.API.Order"/>.
@@ -26,10 +39,29 @@ namespace EasyPost.Parameters.Order
         public IAddressParameter? FromAddress { get; set; }
 
         /// <summary>
+        ///     Whether the new <see cref="Models.API.Order"/> is a return.
+        /// </summary>
+        [TopLevelRequestParameter(Necessity.Optional, "order", "is_return")]
+        public bool? IsReturn { get; set; }
+
+        /// <summary>
+        ///     Additional <see cref="Models.API.Options"/> for the new <see cref="Models.API.Order"/>.
+        /// </summary>
+        [TopLevelRequestParameter(Necessity.Optional, "order", "options")]
+        public Models.API.Options? Options { get; set; }
+
+        /// <summary>
         ///     Reference name for the new <see cref="Models.API.Order"/>.
         /// </summary>
         [TopLevelRequestParameter(Necessity.Optional, "order", "reference")]
         public string? Reference { get; set; }
+
+        /// <summary>
+        ///     The return <see cref="Models.API.Address"/> (or <see cref="Address.Create"/> parameters) for the new <see cref="Models.API.Order"/>.
+        ///     Defaults to <see cref="FromAddress"/> if not provided.
+        /// </summary>
+        [TopLevelRequestParameter(Necessity.Optional, "order", "return_address")]
+        public IAddressParameter? ReturnAddress { get; set; }
 
         /// <summary>
         ///     List of <see cref="Models.API.Shipment"/>s (or <see cref="Shipment.Create"/> parameter sets) for the new <see cref="Models.API.Order"/>.
