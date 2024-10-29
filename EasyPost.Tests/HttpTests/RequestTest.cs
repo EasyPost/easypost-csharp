3622eeb7c97e4736dd719e27d9804446b633ec72
Nate Harris <nwithan8@users.noreply.github.com>
2024-10-29 14:59:16 -0600

    - Handle serializing array/list parameters in GET/DELETE query strings - Unit test to confirm serialization

--- EasyPost.Tests/HttpTests/RequestTest.cs
+++ EasyPost.Tests/HttpTests/RequestTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Net.Http;
 using EasyPost._base;
 using EasyPost.Http;
 using Xunit;
@@ -15,6 +16,38 @@ public class RequestTest
     {
         Request request = new("https://google.com", "not_a_real_endpoint", Method.Get, ApiVersion.V2, new Dictionary<string, object> { { "key", "value" } }, new Dictionary<string, string> { { "header_key", "header_value" } });
         CustomAssertions.DoesNotThrow(() => request.Dispose());
+    }
+
+    [Fact]
+    public void TestQueryParameterList()
+    {
+        const string key = "key";
+        var value = new List<string>
+        {
+            "value1",
+            "value2"
+        };
+        const string key2 = "key2";
+        const string value2 = "value3";
+        var parameters = new Dictionary<string, object>
+        {
+            { key, value },
+            { key2, value2 }
+        };
+
+        Request request = new("https://google.com", "not_a_real_endpoint", Method.Get, ApiVersion.V2, parameters, new Dictionary<string, string> { { "header_key", "header_value" } });
+
+        HttpRequestMessage httpRequestMessage = request.AsHttpRequestMessage();
+
+        string queryString = httpRequestMessage.RequestUri?.Query;
+
+        string keyWithArray = $"{key}[]";
+
+        foreach (string item in value)
+        {
+            Assert.Contains($"{keyWithArray}={item}", queryString); // Does not account for URL encoding
+        }
+        Assert.Contains($"{key2}={value2}", queryString); // Does not account for URL encoding
     }
 
     #endregion
