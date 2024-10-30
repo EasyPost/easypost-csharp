7c2581c236bebfb9b01c574b69f042c241fe9ec2
Nate Harris <nwithan8@users.noreply.github.com>
2024-10-30 16:16:09 -0600

    - Address feedback

--- EasyPost.Tests/HttpTests/RequestTest.cs
+++ EasyPost.Tests/HttpTests/RequestTest.cs
@@ -14,40 +14,28 @@ public class RequestTest
     [Fact]
     public void TestRequestDisposal()
     {
-        Request request = new("https://google.com", "not_a_real_endpoint", Method.Get, ApiVersion.V2, new Dictionary<string, object> { { "key", "value" } }, new Dictionary<string, string> { { "header_key", "header_value" } });
+        Request request = new("https://example.com", "not_a_real_endpoint", Method.Get, ApiVersion.V2, new Dictionary<string, object> { { "key", "value" } }, new Dictionary<string, string> { { "header_key", "header_value" } });
         CustomAssertions.DoesNotThrow(() => request.Dispose());
     }
 
     [Fact]
     public void TestQueryParameterList()
     {
-        const string key = "key";
-        var value = new List<string>
-        {
-            "value1",
-            "value2"
-        };
-        const string key2 = "key2";
-        const string value2 = "value3";
         var parameters = new Dictionary<string, object>
         {
-            { key, value },
-            { key2, value2 }
+            { "key1", new List<string> { "value1", "value2" } },
+            { "key2", "value3" }
         };
 
-        Request request = new("https://google.com", "not_a_real_endpoint", Method.Get, ApiVersion.V2, parameters, new Dictionary<string, string> { { "header_key", "header_value" } });
+        Request request = new("https://example.com", "not_a_real_endpoint", Method.Get, ApiVersion.V2, parameters, new Dictionary<string, string> { { "header_key", "header_value" } });
 
         HttpRequestMessage httpRequestMessage = request.AsHttpRequestMessage();
 
         string queryString = httpRequestMessage.RequestUri?.Query;
 
-        string keyWithArray = $"{key}[]";
-
-        foreach (string item in value)
-        {
-            Assert.Contains($"{keyWithArray}={item}", queryString); // Does not account for URL encoding
-        }
-        Assert.Contains($"{key2}={value2}", queryString); // Does not account for URL encoding
+        Assert.Contains("key1[]=value1", queryString); // Does not account for URL encoding
+        Assert.Contains("key1[]=value2", queryString); // Does not account for URL encoding
+        Assert.Contains("key2=value3", queryString); // Does not account for URL encoding
     }
 
     #endregion
