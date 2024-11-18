d7d0078d69c2151dbdb76e56cc1a16eb516a5e51
Nate Harris <nwithan8@users.noreply.github.com>
2024-11-18 13:24:31 -0700

    - Add .NET 9.0 support

--- scripts/win/setup.bat
+++ scripts/win/setup.bat
@@ -10,7 +10,7 @@
 @ECHO OFF
 
 :: .NET Versions we want to install and destination
-SET NetVersions=Current 8.0 7.0 6.0
+SET NetVersions=Current 9.0 8.0 7.0 6.0
 SET InstallPath=C:\dotnet
 
 :: Dependency file
