e492e2d3291343818c984f6fdfd3c7f70a3526bd
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-03-11 11:59:47 -0600

    fix: netstandard test

--- EasyPost.Compatibility.NetStandard/NetStandardCompileTest.cs
+++ EasyPost.Compatibility.NetStandard/NetStandardCompileTest.cs
@@ -1,13 +1,18 @@
 ﻿// This test checks that EasyPost C# code can be used in NetStandard.
 
+using EasyPost;
+using Xunit;
+
 namespace EasyPost.Compatibility.NetStandard
-
-open EasyPost
-open Xunit
-
-type NetStandardCompileTest() =
-    [<Fact>]
-member this.TestCompile() =
-        let client = new Client(new ClientConfiguration("fake_api_key"))
-        // The assert doesn't really do anything, but as long as this test can run, then the code is compiling correctly.
-        Assert.NotNull(client)
+{
+    public class NetStandardCompileTest
+    {
+        [Fact]
+        public void TestCompile()
+        {
+            var client = new Client(new ClientConfiguration("fake_api_key"));
+            // The assert doesn't really do anything, but as long as this test can run, then the code is compiling correctly.
+            Assert.NotNull(client);
+        }
+    }
+}
